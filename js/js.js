966532d897ddeae91175297ffc528c08139adc53
crystian97 <icrystianveloso@gmail.com>
2022-02-05 16:33:41 -0300

    game loop

--- js/js.js
+++ js/js.js
@@ -1,12 +1,30 @@
-$('#iniciar').on('click',start);
-function start(){
-    // inicia o jogo
-    $("#inicio").hide();
-    $('#iniciar').hide();
-    // adiciona os elementos
-    $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
-    $("#fundoGame").append("<div id='inimigo2' ></div>");
-    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+$("#iniciar").on("click", start);
+function start() {
+  // inicia o jogo
+  $("#inicio").hide();
+  $("#iniciar").hide();
+  // adiciona os elementos
+  $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+  $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+  $("#fundoGame").append("<div id='inimigo2' ></div>");
+  $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+  // game loop
+  //Principais variáveis do jogo
+
+  var jogo = {};
+
+  //Game Loop
+
+  jogo.timer = setInterval(loop, 30);
+
+  function loop() {
+    movefundo();
+  } // Fim da função loop()
+  //Função que movimenta o fundo do jogo
+
+  function movefundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position", esquerda - 1);
+  } // fim da função movefundo()
 }
