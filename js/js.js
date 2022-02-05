295801a8d1686ceff2af84864c528cf3aa0a55e1
crystian97 <icrystianveloso@gmail.com>
2022-02-05 15:08:51 -0300

    adicionando animações

--- js/js.js
+++ js/js.js
@@ -4,9 +4,9 @@ function start(){
     $("#inicio").hide();
     $('#iniciar').hide();
     // adiciona os elementos
-    $("#fundoGame").append("<div id='jogador'></div>");
-    $("#fundoGame").append("<div id='inimigo1'></div>");
-    $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo'></div>");
+    $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+    $("#fundoGame").append("<div id='inimigo2' ></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 }
