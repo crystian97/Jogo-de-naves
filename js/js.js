54e42c5e567eaa4abfa61d7afd806d7e178e793e
crystian97 <icrystianveloso@gmail.com>
2022-02-07 00:16:49 -0300

    movimentação

--- js/js.js
+++ js/js.js
@@ -14,61 +14,87 @@ function start() {
 
   var jogo = {};
 
-  
-	var TECLA = {
-        W: 87,
-        S: 83,
-        D: 68
-        }
-    
-        jogo.pressionou = [];
-    
-    
-        //Verifica se o usuário pressionou alguma tecla	
-        
-        $(document).keydown(function(e){
-        jogo.pressionou[e.which] = true;
-        });
-    
-    
-        $(document).keyup(function(e){
-           jogo.pressionou[e.which] = false;
-        });
-    
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 334);
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68,
+  };
+
+  jogo.pressionou = [];
+
+  //Verifica se o usuário pressionou alguma tecla
+
+  $(document).keydown(function (e) {
+    jogo.pressionou[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    jogo.pressionou[e.which] = false;
+  });
 
   function movejogador() {
-	
-	if (jogo.pressionou[TECLA.W]) {
-    var topo = parseInt($("#jogador").css("top"));
-    $("#jogador").css("top", topo - 10);
-    if (topo <= 0) {
-      $("#jogador").css("top", topo + 10);
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo - 10);
+      if (topo <= 0) {
+        $("#jogador").css("top", topo + 10);
+      }
     }
-  }
-	
-	if (jogo.pressionou[TECLA.S]) {
-		
-		var topo = parseInt($("#jogador").css("top"));
-		$("#jogador").css("top",topo+10);	
-        if (topo>=434) {	
-            $("#jogador").css("top",topo-10);
-                
-        }
-	}
-	
-	if (jogo.pressionou[TECLA.D]) {
-		
-		//Chama função Disparo	
-	}
 
-	} // fim da função movejogador()
+    if (jogo.pressionou[TECLA.S]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo + 10);
+      if (topo >= 434) {
+        $("#jogador").css("top", topo - 10);
+      }
+    }
+
+    if (jogo.pressionou[TECLA.D]) {
+      //Chama função Disparo
+    }
+  } // fim da função movejogador()
+  function moveinimigo1() {
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left", posicaoX - velocidade);
+    $("#inimigo1").css("top", posicaoY);
+
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+    }
+  } //Fim da função moveinimigo1()
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left", posicaoX - 3);
+
+    if (posicaoX <= 0) {
+      $("#inimigo2").css("left", 775);
+    }
+  } // Fim da função moveinimigo2()
+
+  function moveamigo() {
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left", posicaoX + 1);
+
+    if (posicaoX > 906) {
+      $("#amigo").css("left", 0);
+    }
+  } // fim da função moveamigo()
+
   //Game Loop
   jogo.timer = setInterval(loop, 30);
 
   function loop() {
-
     movefundo();
     movejogador();
+
+    moveinimigo1();
+    moveinimigo2();
+
+    moveamigo();
   } // Fim da função loop()
   //Função que movimenta o fundo do jogo
 
@@ -77,4 +103,3 @@ function start() {
     $("#fundoGame").css("background-position", esquerda - 1);
   } // fim da função movefundo()
 }
-
