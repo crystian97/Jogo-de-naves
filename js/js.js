86bd9a45c47156e8e816fa0e7225a04a8e487898
crystian97 <icrystianveloso@gmail.com>
2022-02-10 00:23:57 -0300

    Disparo

--- js/js.js
+++ js/js.js
@@ -13,7 +13,7 @@ function start() {
   //Principais variáveis do jogo
 
   var jogo = {};
-
+  var podeAtirar=true;
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
@@ -33,7 +33,18 @@ function start() {
   $(document).keyup(function (e) {
     jogo.pressionou[e.which] = false;
   });
+  //Game Loop
+  jogo.timer = setInterval(loop, 30);
 
+  function loop() {
+    movefundo();
+    movejogador();
+
+    moveinimigo1();
+    moveinimigo2();
+
+    moveamigo();
+  } // Fim da função loop()
   function movejogador() {
     if (jogo.pressionou[TECLA.W]) {
       var topo = parseInt($("#jogador").css("top"));
@@ -53,6 +64,7 @@ function start() {
 
     if (jogo.pressionou[TECLA.D]) {
       //Chama função Disparo
+      disparo()
     }
   } // fim da função movejogador()
   function moveinimigo1() {
@@ -84,18 +96,38 @@ function start() {
     }
   } // fim da função moveamigo()
 
-  //Game Loop
-  jogo.timer = setInterval(loop, 30);
-
-  function loop() {
-    movefundo();
-    movejogador();
-
-    moveinimigo1();
-    moveinimigo2();
-
-    moveamigo();
-  } // Fim da função loop()
+  function disparo() {
+	
+    if (podeAtirar==true) {
+      
+    podeAtirar=false;
+    
+    topo = parseInt($("#jogador").css("top"))
+    posicaoX= parseInt($("#jogador").css("left"))
+    tiroX = posicaoX + 190;
+    topoTiro=topo+37;
+    $("#fundoGame").append("<div id='disparo'></div");
+    $("#disparo").css("top",topoTiro);
+    $("#disparo").css("left",tiroX);
+    
+    var tempoDisparo=window.setInterval(executaDisparo, 30);
+    
+    } //Fecha podeAtirar
+   
+           function executaDisparo() {
+        posicaoX = parseInt($("#disparo").css("left"));
+        $("#disparo").css("left",posicaoX+15); 
+  
+              if (posicaoX>900) {
+              
+        window.clearInterval(tempoDisparo);
+        tempoDisparo=null;
+        $("#disparo").remove();
+        podeAtirar=true;
+            
+                     }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
   //Função que movimenta o fundo do jogo
 
   function movefundo() {
