c6da28bdce466494a1c7bc8a1c564f675266ef1a
crystian97 <icrystianveloso@gmail.com>
2022-02-10 20:50:43 -0300

    Colisão Inimigo 01

--- js/js.js
+++ js/js.js
@@ -132,10 +132,42 @@ function start() {
   function colisao() {
     var colisao1 = ($("#jogador").collision($("#inimigo1")));
     // jogador com o inimigo1
-  
-    console.log(colisao1);
-  
-  } //Fim da função colisao()
+      
+      if (colisao1.length>0) {
+        
+      inimigo1X = parseInt($("#inimigo1").css("left"));
+      inimigo1Y = parseInt($("#inimigo1").css("top"));
+      explosao1(inimigo1X,inimigo1Y);
+    
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left",694);
+      $("#inimigo1").css("top",posicaoY);
+      }
+    
+    } //Fim da função colisao()
+    
+  //Explosão 1
+function explosao1(inimigo1X,inimigo1Y) {
+	$("#fundoGame").append("<div id='explosao1'></div");
+  // indicar imagem direto no código do javascript(pois não funciona totalmente no css )
+	$("#explosao1").css("background-image", "url(imgs/explosao.png)");
+	var div=$("#explosao1");
+	div.css("top", inimigo1Y);
+	div.css("left", inimigo1X);
+  // animação da explosão
+	div.animate({width:200, opacity:0}, "slow");
+	
+	var tempoExplosao=window.setInterval(removeExplosao, 1000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+			
+		}
+		
+	} // Fim da função explosao1()
   //Função que movimenta o fundo do jogo
 
   function movefundo() {
