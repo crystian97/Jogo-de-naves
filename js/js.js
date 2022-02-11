5ca262dc89b1d9f2c4af58f1a1de5aaa259238f1
crystian97 <icrystianveloso@gmail.com>
2022-02-10 23:09:00 -0300

    aumentando a velocidade do primeiro inimigo

--- js/js.js
+++ js/js.js
@@ -22,6 +22,8 @@ function start() {
   var salvos = 0;
   var perdidos = 0;
 
+  var energiaAtual=3;
+
   var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
     W: 87,
@@ -53,6 +55,7 @@ function start() {
 
     moveamigo();
     placar();
+    energia();
   } // Fim da função loop()
   function movejogador() {
     if (jogo.pressionou[TECLA.W]) {
@@ -143,6 +146,8 @@ function start() {
     // jogador com o inimigo1
 
     if (colisao1.length > 0) {
+      energiaAtual--;
+
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
       explosao1(inimigo1X, inimigo1Y);
@@ -153,6 +158,8 @@ function start() {
     }
     // jogador com o inimigo2
     if (colisao2.length > 0) {
+      energiaAtual--;
+
       inimigo2X = parseInt($("#inimigo2").css("left"));
       inimigo2Y = parseInt($("#inimigo2").css("top"));
       explosao2(inimigo2X, inimigo2Y);
@@ -165,7 +172,7 @@ function start() {
 
     if (colisao3.length > 0) {
       pontos=pontos+100;
-
+      velocidade=velocidade+0.3;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -297,5 +304,29 @@ function start() {
     $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
     
   } //fim da função placar()
-  
+  function energia() {
+	
+		if (energiaAtual==3) {
+			
+			$("#energia").css("background-image", "url(imgs/energia3.png)");
+		}
+	
+		if (energiaAtual==2) {
+			
+			$("#energia").css("background-image", "url(imgs/energia2.png)");
+		}
+	
+		if (energiaAtual==1) {
+			
+			$("#energia").css("background-image", "url(imgs/energia1.png)");
+		}
+	
+		if (energiaAtual==0) {
+			
+			$("#energia").css("background-image", "url(imgs/energia0.png)");
+			
+			//Game Over
+		}
+	
+	} // Fim da função energia()
 }
